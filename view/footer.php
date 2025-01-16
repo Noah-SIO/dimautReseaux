17e23fef121496212ec8d43d7e4bc6b5aededb9a
Noah_FGC <150355925+Noah-SIO@users.noreply.github.com>
2025-01-16 16:41:50 +0100

    ajout img dans footer

--- view/footer.php
+++ view/footer.php
@@ -4,3 +4,4 @@ echo"<hr>";
 echo"<p>Date de création : 15/01/2025</p>";
 echo"<p>Liens du GitHub : <a href='https://github.com/Noah-SIO/dimAutReseaux'>GitHub DimAutReseaux</a></p>";
 echo"</div>";
+echo"</br><img src='img/DIM.png' width='129.5' height='53' style='display: block; margin: 0 auto;'>";
