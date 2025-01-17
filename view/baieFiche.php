4242655806836f4667ec51d3e86792fcac94fb22
Noah_FGC <150355925+Noah-SIO@users.noreply.github.com>
2025-01-17 14:29:04 +0100

    ajout nombre port et type de liaison sur fiche baie

--- view/baieFiche.php
+++ view/baieFiche.php
@@ -9,5 +9,6 @@ echo"<h1>". $tableaubaie[0]->getNom() . "</h1>";
 
 for($i=0; $i< count($tableauswitch); $i++){
     echo"<p> Switch ". $tableauswitch[$i]->getMarque() . " " . $tableauswitch[$i]->getModele() . " :". "</p>";
+    echo"<p>Nombre de port : " . $tableauswitch[$i]->getNbPort() . "  et Type de Liaison : " . $tableauswitch[$i]->getTypeLiaison() ."</p>";
     echo"<img src='". $tableauswitch[$i]->getImg() . "'>";
 }
