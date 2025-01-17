3bf11b7b6b7b74421bb1f4d8b1431b8580d00f7a
Noah_FGC <150355925+Noah-SIO@users.noreply.github.com>
2025-01-17 14:29:22 +0100

    modif methode baie

--- class/baie.php
+++ class/baie.php
@@ -93,7 +93,7 @@ class ManagerBaie{
         if($donneesBaie != NULL){
             for ($i=0 ; $i<count($donneesBaie) ;$i++){
                 $tableauBaie[]= new Baie($donneesBaie['id'],$donneesBaie['id_batiments'],
-                $donneesBaie['nom'],$donneesBaie['pourcentage_utilisation'],$donneesBaie['type liaison'],$donneesBaie['nb_switch'],$donneesBaie['img']);                    
+                $donneesBaie['nom'],$donneesBaie['pourcentage_utilisation'],$donneesBaie['type_liaison'],$donneesBaie['nb_switch'],$donneesBaie['img']);                    
             }                                   
         return $tableauBaie;
     }
@@ -107,7 +107,7 @@ class ManagerBaie{
         $tableauBaie= array();
         if($donneesBaie != NULL){      
             $tableauBaie[]= new Baie($donneesBaie['id'],$donneesBaie['id_batiments'],
-            $donneesBaie['nom'],$donneesBaie['pourcentage_utilisation'],$donneesBaie['type liaison'],$donneesBaie['nb_switch'],$donneesBaie['img']);                             
+            $donneesBaie['nom'],$donneesBaie['pourcentage_utilisation'],$donneesBaie['type_liaison'],$donneesBaie['nb_switch'],$donneesBaie['img']);                             
         //var_dump($tableauSearchByID);
         return $tableauBaie;
     }
